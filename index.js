06d69ea52b4cc5e77ee86fa49505550aeda67669
fabegalo <fabegalo@gmail.com>
2022-07-19 17:04:14 -0300

    teste

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ app.use('/request-type', (req, res, next) => {
   next();
 });
 
+app.use(express.static(__dirname + '/public'));
+
 app.use('/public', express.static('public'));
 app.use('/public', serveIndex('public'));
 
